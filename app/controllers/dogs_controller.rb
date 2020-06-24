4847508203c6a493afd9ebd3b33582ebe43d5e35
DannyCoop <cooperdaniel5141@gmail.com>
2020-06-24 15:09:09 -0400

    set up the index and show html as well as added a dog id column to the employee

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,11 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all
+        byebug
+    end
+    
+    def show
+        # byebug
+        @dog = Dog.find(params[:id])
+    end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,9 @@
 class Dog < ApplicationRecord
+    has_many :employees
+
+
+    def associatied_employees
+
+    end
+
 end
