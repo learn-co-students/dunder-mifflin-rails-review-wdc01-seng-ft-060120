f81898a4dd98392e3a1d029501389faa57c061b6
Issa sangare <issabmsangare@hotmail.com>
2020-06-24 17:37:30 -0400

    done

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -8,4 +8,32 @@ class EmployeesController < ApplicationController
         @employee = Employee.find(params[:id])
     end 
 
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        # byebug
+        @employee = Employee.new(params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url))
+        @employee.save
+        redirect_to employee_path(@employee)
+    end
+
+    def edit
+       
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(first_name: params[:employee][:first_name],
+                         last_name: params[:employee][:last_name],
+                          alias: params[:employee][:alias],
+                          title: params[:employee][:title],
+                          office: params[:employee][:office],
+                          dog_id: params[:employee][:dog_id],
+                          img_url: params[:employee][:img_url] )
+        redirect_to employee_path(@employee.id)
+
+    end
 end
