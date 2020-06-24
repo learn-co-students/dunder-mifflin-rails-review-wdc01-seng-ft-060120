d2043d021d9d5680a3de20c611a3396319c351d3
mjester93 <m.jester.93@gmail.com>
2020-06-24 15:11:36 -0400

    add

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,11 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        id = params[:id]
+        @dog = Dog.find(id)
+    end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,23 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        id = params[:id]
+        @employee = Employee.find(id)
+    end
+
+    def new
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+    end
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :office, :img_url, :dog)
+    end
 end
